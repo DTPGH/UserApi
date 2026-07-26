fa6e8721169a2bf6e442d7bb4ddc333a33e872de
DatPGH <datphan0602@gmail.com>
2026-07-27 00:18:46 +0700

    add field role in dtos/response/userresponse

--- DTOs/Responses/UserResponse.cs
+++ DTOs/Responses/UserResponse.cs
@@ -5,4 +5,5 @@ public class UserResponse
     public string Email { get; set; } = "";
     public string? Description { get; set; }
     public int Age { get; set; }
-} 
+    public string Role { get; set; } = "";
+}
